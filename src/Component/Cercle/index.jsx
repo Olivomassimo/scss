12467cb37180643fa5c60807e2a00a9ad79e0dd3
massimo olivo <massimo.olivo@gmail.com>
2019-05-06 20:29:02 +0200

    scss

---
+++ src/Component/Cercle/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './index.scss'
+
+const Cercle = () => {
+    return (
+        <div>
+            <div className="container-cercle1">
+                <div className="cercle8"></div>
+                <div><h2>The one ring</h2></div>
+                <div className="cercle8"></div>
+            </div>
+            <div className="container-cercle1">
+                <div className="cercle9"></div>
+                <h1>My precions</h1>
+            </div>
+            <div className="container-cercle">
+                <div className="container">
+                    <div className="circle1"></div>
+                    <div className="circle2"></div>
+                    <div className="circle3"></div>
+                    <div className="circle4"></div>
+                    <div className="circle5"></div>
+                    <div className="circle6"></div>
+                    <div className="circle7"></div>
+                </div>
+            </div>
+            <div className="la-fin">
+                <h2>Please Find It !</h2>
+            </div>
+        </div>
+    );
+};
+export default Cercle
