2d7b9d36f7bd93d12458a7ad43089c81f53b19be
massimo olivo <massimo.olivo@gmail.com>
2019-05-13 12:25:49 +0200

    Updates

---
+++ precache-manifest.21a9aae0b81fcd4472c5cd0eb098d16b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a390ba60793e357386d0e94bb4d8ca8c",
+    "url": "/scss/index.html"
+  },
+  {
+    "revision": "a1a541e6ebcda71315e9",
+    "url": "/scss/static/css/main.b1f639a5.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/scss/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "a1a541e6ebcda71315e9",
+    "url": "/scss/static/js/main.83df2c47.chunk.js"
+  },
+  {
+    "revision": "151646fab342214a43d0",
+    "url": "/scss/static/js/runtime~main.5f55a73a.js"
+  }
+]);
